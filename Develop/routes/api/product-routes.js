b547b52f94845e52a8f1ec0a5a7b71e0cdf0b7f2
Mandeep Aulakh <119018981+Mdwag316@users.noreply.github.com>
2023-02-24 16:52:48 -0800

    Update product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,7 +6,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll( {include: [
       {model: Category},
@@ -22,7 +21,6 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {include: [
       {model: Category},
